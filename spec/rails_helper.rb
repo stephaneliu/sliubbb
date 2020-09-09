eb6fd77ce678a7a2d7c12455f26770ce9298f9de
Stephane Liu <sliu@sjliu.com>
2020-09-08 19:50:35 -1000

    Fix: Ordering error of RAILS_ENV
      - Setting to test environment has to happen before loading envir
        otherwise test env does not load correctly. This was causing strange
        failures in rspec request tests.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,9 +2,9 @@
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require "spec_helper"
+ENV["RAILS_ENV"] ||= "test"
 require File.expand_path("../config/environment", __dir__)
 
-ENV["RAILS_ENV"] ||= "test"
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 
 require "rspec/rails"
